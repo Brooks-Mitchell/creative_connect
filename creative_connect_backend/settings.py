a206443563bc34c73f95179fcd6f5ba5935070c5
Bemly <evan93.mitchell@gmail.com>
2022-10-15 15:10:31 -0600

    connect JS frontend and PostgreSQL database

--- creative_connect_backend/settings.py
+++ creative_connect_backend/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'creative_connect_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'creative_connect',
+        'USER' : 'postgres',
+        'PASSWORD' : '2514',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
