6e8877b0bccad0342065497ce0543283f145fc25
Bemly <evan93.mitchell@gmail.com>
2022-10-16 15:45:08 -0600

    Start user posts app, migrate db

---
+++ user_posts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# Create your models here.
+class User_Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=320)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
